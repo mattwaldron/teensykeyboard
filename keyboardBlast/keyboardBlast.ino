9e9d46dec28d2cf1d47a1241543262ae2df56df1
matt <waldron.matthew@gmail.com>
2018-12-06 08:37:31 -0600

    initial commit

---
+++ keyboardBlast/keyboardBlast.ino
@@ -0,0 +1,27 @@
+#include <Bounce.h>
+
+const int buttonPin = 12;
+Bounce pushbutton = Bounce(buttonPin, 10);  // 10 ms debounce
+
+void KeyboardPressAndRelease(int key) {
+  Keyboard.press(key);
+  Keyboard.release(key);
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
+}
+
+void loop() {
+  if (pushbutton.update()) {
+    if (pushbutton.fallingEdge()) {
+      Keyboard.print("Hello Matt");
+      Keyboard.press(MODIFIERKEY_ALT);
+      KeyboardPressAndRelease(KEY_E);
+      KeyboardPressAndRelease(KEY_M);
+      Keyboard.release(MODIFIERKEY_ALT);
+      KeyboardPressAndRelease(KEY_ENTER);
+    }
+  } 
+}
+
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Installation
+Download Arduino IDE at
+https://www.arduino.cc/download_handler.php
+
+Download Teensyduino at
+https://www.pjrc.com/teensy/td_download.html
+
+# Building and Loading
+1. Load the ino with the Arduino IDE.
+2. Make sure 'Keyboard' is selected under Tools>USB Type
+3. Press Upload
